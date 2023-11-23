2193ab28bb60166d83a992a4bc00fe8a3393e25c
Karol Jurzec <85733696+karol-jurzec@users.noreply.github.com>
2023-11-23 23:53:33 +0100

    ant colony, basic ant and antRoad class

--- src/main/java/tsp/algorithms/AntColonyAlgorithm.java
+++ src/main/java/tsp/algorithms/AntColonyAlgorithm.java
@@ -1,4 +1,30 @@
 package src.main.java.tsp.algorithms;
 
+
+import src.main.java.tsp.models.City;
+import src.main.java.tsp.models.Road;
+
 public class AntColonyAlgorithm {
+    class Ant {
+
+        private City currentCity;
+
+
+
+    }
+
+    class AntRoad extends Road {
+
+        public AntRoad(City destination, float weight) {
+            super(destination, weight);
+        }
+
+        private float pheromoneAmount;
+
+    }
+
+    //matrix with list of pheromones?
+    //matrx with list of distances?
+
+
 }
