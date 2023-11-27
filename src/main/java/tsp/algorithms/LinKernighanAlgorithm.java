8af42228e0456a97b7c85ac812f758db2cf04d0f
Karol Jurzec <85733696+karol-jurzec@users.noreply.github.com>
2023-11-27 23:05:59 +0100

    ant colony, fix

---
+++ src/main/java/tsp/algorithms/LinKernighanAlgorithm.java
@@ -0,0 +1,19 @@
+package src.main.java.tsp.algorithms;
+
+import src.main.java.tsp.models.City;
+import src.main.java.tsp.models.TspSolution;
+
+import java.util.ArrayList;
+
+public class LinKernighanAlgorithm implements ITspAlgorithm {
+
+    @Override
+    public TspSolution solve(ArrayList<City> cities) {
+        return null;
+    }
+
+    @Override
+    public void printAlgorithm(ArrayList<ArrayList<City>> steps) {
+
+    }
+}
